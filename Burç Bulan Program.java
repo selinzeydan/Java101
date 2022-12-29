16bfb9ef35d813a9ca4f91c4ab951d4547ddbe93
Selin Zeydan <116116043+selinzeydan@users.noreply.github.com>
2022-12-29 13:46:13 +0300

    Update Burç Bulan Program.java

--- Burç Bulan Program.java
+++ Burç Bulan Program.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class BurcBulanProgram {
     public static void main(String[] args) {
        int mounth, day;
 
@@ -13,29 +13,29 @@ public class Main {
         day = input.nextInt();
 
         if ((mounth == 12) && (day >= 22) || ((mounth == 1) && (day <= 21))) {
-            System.out.println("Oglak Burcu");
+            System.out.println("Burcunuz: Oglak Burcu");
         } else if ((mounth == 1) && (day >= 22) || ((mounth == 2) && (day <= 19))) {
-            System.out.println("Kova Burcu");
+            System.out.println("Burcunuz: Kova Burcu");
         } else if ((mounth == 2) && (day >= 20) || ((mounth == 3) && (day <= 20))) {
-            System.out.println("Balik Burcu");
+            System.out.println("Burcunuz: Balik Burcu");
         } else if ((mounth == 3) && (day >= 21) || ((mounth == 4) && (day <= 20))) {
-            System.out.println("Koc Burcu");
+            System.out.println("Burcunuz: Koc Burcu");
         } else if ((mounth == 4) && (day >= 21) || ((mounth == 5) && (day <= 21))) {
-            System.out.println("Boga Burcu");
+            System.out.println("Burcunuz: Boga Burcu");
         } else if ((mounth == 5) && (day >= 22) || ((mounth == 6) && (day <= 22))) {
-            System.out.println("Ikizler Burcu");
+            System.out.println("Burcunuz: Ikizler Burcu");
         } else if ((mounth == 6) && (day >= 23) || ((mounth == 7) && (day <= 22))) {
-            System.out.println("Yengec Burcu");
+            System.out.println("Burcunuz: Yengec Burcu");
         } else if ((mounth == 7) && (day >= 23) || ((mounth == 8) && (day <= 22))) {
-            System.out.println("Aslan Burcu");
+            System.out.println("Burcunuz: Aslan Burcu");
         } else if ((mounth == 8) && (day >= 23) || ((mounth == 9) && (day <= 22))) {
-            System.out.println("Basak Burcu");
+            System.out.println("Burcunuz: Basak Burcu");
         } else if ((mounth == 9) && (day >= 23) || ((mounth == 10) && (day <= 22))) {
-            System.out.println("Terazi Burcu");
+            System.out.println("Burcunuz: Terazi Burcu");
         } else if ((mounth == 10) && (day >= 23) || ((mounth == 11) && (day <= 21))) {
-            System.out.println("Akrep Burcu");
+            System.out.println("Burcunuz: Akrep Burcu");
         } else if ((mounth == 11) && (day >= 22) || ((mounth == 12) && (day <= 21))) {
-            System.out.println("Yay Burcu");
+            System.out.println("Burcunuz: Yay Burcu");
         } else if ((mounth == 12) && (day >= 22) || ((mounth == 1) && (day <= 21))) {
-            System.out.println("Oglak Burcu");
+            System.out.println("Burcunuz: Oglak Burcu");
       
